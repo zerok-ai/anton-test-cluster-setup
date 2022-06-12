c8826954dd5b3e2dd50937150163c78f0016e3a0
Mudit Krishna Mathur <muditkmathur@gmail.com>
2022-06-12 20:07:40 +0530

    Two instances of nginx

--- eks/infra/install-ingress-controller.sh
+++ eks/infra/install-ingress-controller.sh
@@ -5,15 +5,15 @@ echo '---------------------- Updating helm repo for ingress-nginx'
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
 
-# Create namespace
-echo '---------------------- Creating namespace `ingress`'
-kubectl create namespace ingress
-
 # Install the Helm (v3) chart for nginx ingress controller
 echo '---------------------- Installing helm chart of ingress-nginx'
 helm upgrade --install app-ingress ingress-nginx/ingress-nginx \
-	--namespace ingress \
+	--create-namespace --namespace ingress \
 	--values ./yaml/values/nginx-ingress-values.yaml
+
+helm upgrade --install anton-ingress ingress-nginx/ingress-nginx \
+	--create-namespace --namespace anton-ingress \
+	--values ./yaml/values/anton-nginx-ingress-values.yaml
 
 # Print the Ingress Controller public IP address
 kubectl get services --namespace ingress
