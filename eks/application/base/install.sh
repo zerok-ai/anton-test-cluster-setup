3b53eb3ede51031dcd11a5a62eb8532f82867817
Shivam Nagar <shivam.nagar@outlook.com>
2022-06-12 19:33:14 +0530

    Cleanup

--- eks/application/base/install.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-kubectl apply -k .
-
-# modify kustomization.yaml
--- eks/application/base/uninstall.sh
+++
@@ -1,6 +0,0 @@
-kubectl delete -f services.yaml
-kubectl delete -f deployment.yaml
-kubectl delete -f ingress.yaml
-kubectl delete -f service-monitor.yaml
-
-#kubectl delete -f autoscaler.yaml
