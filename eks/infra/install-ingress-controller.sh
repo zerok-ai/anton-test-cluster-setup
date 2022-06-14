c391251162967a2d467bbc891a79da8a4cb316ed
Shivam Nagar <shivam.nagar@outlook.com>
2022-06-14 15:40:14 +0530

    replica and VPA changes for anton setup.

--- eks/infra/install-ingress-controller.sh
+++ eks/infra/install-ingress-controller.sh
@@ -11,14 +11,8 @@ helm upgrade --install app-ingress ingress-nginx/ingress-nginx \
 	--create-namespace --namespace app-ingress \
 	--values ./yaml/values/app-nginx-ingress-values.yaml
 
-helm upgrade --install anton-ingress ingress-nginx/ingress-nginx \
-	--create-namespace --namespace anton-ingress \
-	--values ./yaml/values/anton-nginx-ingress-values.yaml
-
 # Print the Ingress Controller public IP address
 kubectl get services --namespace app-ingress
-kubectl get services --namespace anton-ingress
-
 #----------- 
 # helm upgrade app-ingress ingress-nginx/ingress-nginx \
 # 	--namespace ingress \
