89d17ad3436158b8bd4c17aafa7c92198b84050e
Shivam Nagar <shivam.nagar@outlook.com>
2022-06-19 22:50:09 +0530

    Added APIs for xk6

---
+++ xk6-apis/index.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const fs = require('fs');
+const app = express()
+const port = 3000
+
+const execute = require('child_process').exec
+var running = false;
+app.get('/', (req, res) => {
+    if(running) {
+        status((data) => res.send(data.toString()));
+        return;
+    }
+    running = true;
+    try {
+        run();
+        res.send('Started');
+    } catch(error) {
+        running = false;
+        res.send(error);
+        return;
+    }
+})
+
+app.get('/status', (req, res) => {
+    status((data) => res.send(data.toString()));
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+async function run() {
+    try {
+        console.log("init test run");
+        // const passwdContent = await execute("cat /etc/passwd");
+        execute('sh ./run_xk6.sh', (err, stdout, stderr) => {
+            console.log(err, stdout, stderr)
+            if(err === null) {
+                running = false;
+            }
+        })
+    } catch (error) {
+        console.error(error.toString());
+    }
+}
+
+async function status(callback) {
+    fs.readFile('./lastrun.log', function read(err, data) {
+        if (err) {
+            throw err;
+        }
+        content = data;
+        const template = "<html><body><pre> " + content + "</pre></body></html>";
+        callback(template);
+    });
+}
---
+++ xk6-apis/run_xk6.sh
@@ -0,0 +1,3 @@
+rm lastrun.log
+cd ../xk6
+./run.sh >> ../xk6-apis/lastrun.log
