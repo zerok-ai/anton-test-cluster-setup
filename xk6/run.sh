9849315defa46a62d5e4474c8e7b51151394edcc
Mudit Krishna Mathur <muditkmathur@gmail.com>
2022-06-30 17:40:16 +0530

    Minor changes

--- xk6/run.sh
+++ xk6/run.sh
@@ -1,11 +1,3 @@
-
-ulimit -n 65536
-
-# HOSTNAME=$(aws elb describe-load-balancers | jq -r '.LoadBalancerDescriptions[0].CanonicalHostedZoneName')
-# echo $HOSTNAME
-
-kubectl port-forward service/prom-kube-prometheus-stack-prometheus -n monitoring 9090:9090 &
-
 K6_PROMETHEUS_REMOTE_URL=http://prom-kube-prometheus-stack-prometheus.monitoring.svc.cluster.local:9090/api/v1/write \
 ./k6 run script.js \
     -o output-prometheus-remote \
