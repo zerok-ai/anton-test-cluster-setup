25d60abd8202a612808f62bbb30488b53c8c3b2b
Mudit Krishna Mathur <muditkmathur@gmail.com>
2022-10-20 09:22:31 +0530

    Parameterized the name of the project

--- setup/gke/install.sh
+++ setup/gke/install.sh
@@ -1,14 +1,39 @@
+#!/bin/bash
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 -p [Name of the project (optional parameter)]"
+   exit 1 # Exit script after printing help
+}
+
+# while getopts "a:b:c:" opt
+while getopts "p" opt
+do
+   case "$opt" in
+      p ) project="$OPTARG" ;;
+      h ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
+
+
+if [ -z "$project" ]
+then
+   project="black-scope-358204"
+fi
+
 #create cluster
 echo '---------------------- Creating cluster'
+exit 1
 
 gcloud beta container \
-    --project "black-scope-358204" clusters create "zerok-demo-cluster" \
+    --project "$project" clusters create "zerok-demo-cluster" \
     --zone "us-central1-c" --no-enable-basic-auth --cluster-version "1.22.11-gke.400" \
     --release-channel "regular" --machine-type "e2-medium" --image-type "COS_CONTAINERD" \
     --disk-type "pd-standard" --disk-size "100" --metadata disable-legacy-endpoints=true \
     --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
     --max-pods-per-node "110" --num-nodes "3" --logging=SYSTEM,WORKLOAD --monitoring=SYSTEM \
-    --enable-ip-alias --network "projects/black-scope-358204/global/networks/default" --subnetwork "projects/black-scope-358204/regions/us-central1/subnetworks/default" \
+    --enable-ip-alias --network "projects/$project/global/networks/default" --subnetwork "projects/$project/regions/us-central1/subnetworks/default" \
     --no-enable-intra-node-visibility --default-max-pods-per-node "110" --no-enable-master-authorized-networks \
     --addons HorizontalPodAutoscaling,HttpLoadBalancing,GcePersistentDiskCsiDriver \
     --enable-autoupgrade --enable-autorepair --max-surge-upgrade 1 --max-unavailable-upgrade 0 --enable-shielded-nodes \
